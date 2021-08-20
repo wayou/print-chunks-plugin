2bb103571d68dc5713e6665de9dfe0306f45c2e8
wayou <liuwayong@gmail.com>
2021-08-20 23:12:45 +0800

    fix #1

--- README.md
+++ README.md
@@ -7,9 +7,7 @@ Outputs exactly what went into each chunk
 
 The code is taken from the [webpack documentation](https://github.com/webpack/docs/wiki/plugins#after-optimize-chunk-assetschunks-chunk)
 
-
 I find this so useful for debugging. But there's no package in the npm.
-
 
 ### install
 
@@ -20,13 +18,21 @@ npm i -D print-chunks-plugin
 ### usage
 
 ```js
-var PrintChunksPlugin = require('print-chunks-plugin');
+var PrintChunksPlugin = require("print-chunks-plugin");
 var webpackConfig = {
-  entry: 'index.js',
+  entry: "index.js",
   output: {
-    path: __dirname + '/dist',
-    filename: 'index_bundle.js'
+    path: __dirname + "/dist",
+    filename: "index_bundle.js",
   },
-  plugins: [new PrintChunksPlugin()]
+  plugins: [new PrintChunksPlugin()],
+  // or with option:
+  plugins: [new PrintChunksPlugin({ showFull: true })],
 };
 ```
+
+### Options
+
+- `showFull`: stringify the result to get rid of the 100 array output limit of `console.log`, [learn more](https://stackoverflow.com/questions/41669039/dumping-whole-array-console-log-and-console-dir-output-num-more-items)
+  - type: `bool`
+  - default: `false`
--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 /**
  * outputs exactly what went into each chunk
- * 
+ *
  * code is taken from the webpack documentation
  * https://github.com/webpack/docs/wiki/plugins#after-optimize-chunk-assetschunks-chunk
  */
 
-var PrintChunksPlugin = function() {};
-PrintChunksPlugin.prototype.apply = function(compiler) {
-    compiler.plugin('compilation', function(compilation, params) {
-        compilation.plugin('after-optimize-chunk-assets', function(chunks) {
-            console.log(
-                chunks.map(function(c) {
-                    return {
-                        id: c.id,
-                        name: c.name,
-                        includes: c.modules.map(function(m) {
-                            return m.request;
-                        })
-                    };
-                })
-            );
-        });
+var util = require("util");
+
+var PrintChunksPlugin = function (options = {}) {
+  this.options = options;
+};
+PrintChunksPlugin.prototype.apply = function (compiler) {
+  var showFull = this.options.showFull;
+  compiler.plugin("compilation", function (compilation, params) {
+    compilation.plugin("after-optimize-chunk-assets", function (chunks) {
+      var items = chunks.map(function (c) {
+        return {
+          id: c.id,
+          name: c.name,
+          includes: c.modules.map(function (m) {
+            return m.request;
+          }),
+        };
+      });
+      console.log(
+        showFull ? util.inspect(items, { maxArrayLength: null }) : items
+      );
     });
+  });
 };
 
 module.exports = PrintChunksPlugin;
